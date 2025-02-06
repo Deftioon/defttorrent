c621f53859d3f2f5aa17e8dd49a23e89e78019fd
Jake Lu <jylu1205@outlook.com>
2025-02-07 00:17:41 +0800

    say no to gtk we usin tauri now

---
+++ src/SecondPage.tsx
@@ -0,0 +1,7 @@
+export default function Second() {
+    return (
+        <main className="container">
+            <h1>So you found me</h1>
+        </main>
+    )
+}
--- src/lib.rs
+++
@@ -1,3 +0,0 @@
-pub mod backend;
-pub mod requests;
-pub mod gtk;
